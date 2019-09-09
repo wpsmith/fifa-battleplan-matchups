9c90f378effa05bb22117fcf5f0261646c01aafc
Travis Smith <travis.smith@weather.com>
2019-09-08 22:59:07 -0400

    Update install script

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 sudo yum update -y
+sudo yum install git -y
 sudo yum install -y golang
 
 echo 'export GOPATH=/home/ec2-user/go' >>~/.bash_profile
 echo 'export GOROOT=/usr/lib/golang' >>~/.bash_profile
 echo 'export GOTOOLDIR=/usr/lib/golang/pkg/tool/linux_amd64' >>~/.bash_profile
+source ~/.bash_profile
 
 go version
 go env
